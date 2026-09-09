b4250b185c907426c8740ff546f117fc1cc2e834
Matias Agudelo <agudelomatias0212@gmail.com>
2026-09-09 14:07:22 -0500

    feat: Defino estructura de la ventana con BorderLayout

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -10,5 +10,22 @@ public class VentanaPrincipal extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
+        setLayout(new BorderLayout());
+
+        JPanel panelFormulario = new JPanel();
+        panelFormulario.setBackground(Color.PINK);
+        panelFormulario.setPreferredSize(new Dimension(800, 120));
+
+        JPanel panelTabla = new JPanel();
+        panelTabla.setBackground(Color.CYAN);
+
+        JPanel panelBotones = new JPanel();
+        panelBotones.setBackground(Color.ORANGE);
+        panelBotones.setPreferredSize(new Dimension(800, 60));
+
+        add(panelFormulario, BorderLayout.NORTH);
+        add(panelTabla, BorderLayout.CENTER);
+        add(panelBotones, BorderLayout.SOUTH);
+
     }
 }
