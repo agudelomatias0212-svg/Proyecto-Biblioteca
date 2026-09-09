abe59c0fe6a0c172cdf90c6750a2e7cca1166127
Matias Agudelo <agudelomatias0212@gmail.com>
2026-09-09 13:52:33 -0500

    feat: agrego ventana principal vacia

---
+++ src/VentanaPrincipal.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class VentanaPrincipal extends JFrame {
+
+    public VentanaPrincipal() {
+        setTitle("Biblioteca municipal");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+    }
+}
