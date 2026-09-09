497a2e2953e3220c160063681c32baf101b1be27
Matias Agudelo <agudelomatias0212@gmail.com>
2026-09-09 15:14:06 -0500

    feat:agrego tabla de libros con datos prueba

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.table.DefaultTableModel;
 
 
 public class VentanaPrincipal extends JFrame {
+
+    private DefaultTableModel modeloTabla;
+    private JTable tablaLibros;
 
     public VentanaPrincipal() {
         setTitle("Biblioteca municipal");
@@ -17,11 +21,23 @@ public class VentanaPrincipal extends JFrame {
         panelFormulario.setPreferredSize(new Dimension(800, 120));
 
         JPanel panelTabla = new JPanel();
-        panelTabla.setBackground(Color.CYAN);
+        panelTabla.setLayout(new BorderLayout());
 
         JPanel panelBotones = new JPanel();
         panelBotones.setBackground(Color.ORANGE);
         panelBotones.setPreferredSize(new Dimension(800, 60));
+
+        String[] columnas = {"Titulo", "Autor", "ISBN", "Genero","Año", "Copias"};
+
+        modeloTabla = new DefaultTableModel(columnas, 0);
+        tablaLibros = new JTable(modeloTabla);
+        JScrollPane scrollTabla = new JScrollPane(tablaLibros);
+
+        panelTabla.add(scrollTabla, BorderLayout.CENTER);
+
+        modeloTabla.addRow(new Object[]{"Cien años de soledad", "Gabriel García Márquez", "9780307474728", "Novela", 1967, 3});
+        modeloTabla.addRow(new Object[]{"El coronel no tiene quien le escriba", "Gabriel García Márquez", "9780307475473", "Novela", 1961, 2});
+        modeloTabla.addRow(new Object[]{"La vorágine", "José Eustasio Rivera", "9789583001093", "Novela", 1924, 1});
 
         add(panelFormulario, BorderLayout.NORTH);
         add(panelTabla, BorderLayout.CENTER);
