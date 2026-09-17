945cb4d7782bb0af8f18e9a55077ad7c4951564a
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-09-17 13:46:25 -0500

    feat: implementar registro y validacion de libros

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -12,4 +12,40 @@ public class Biblioteca {
                 "Novela","Ciencia","Historia","Infantil","Tecnico"
         };
 
+        public boolean agregarlibro(Libro libro) {
+
+                if (libro == null) {
+                        return false;
+                }
+                if (libro.getAutor().isBlank() ||
+                        libro.getTitulo().isBlank() ||
+                        libro.getGenero().isBlank() ||
+                        libro.getCodigo().isBlank()) {
+                        return false;
+                }
+
+                int anioActual = java.time.Year.now().getValue();
+
+                if (libro.getañopublicado() <= 0 ||
+                        libro.getañopublicado() > anioActual) {
+                        return false;
+                }
+
+                if (libro.getcopiasDisponibles() < 0){
+                        return false;
+                }
+
+                if (codigosRegistrados.contains(libro.getCodigo())) {
+                        return false;
+                }
+
+                libros.add(libro);
+
+                codigosRegistrados.add(libro.getCodigo());
+
+                librosPorAutor.computeIfAbsent(libro.getAutor(), k -> new ArrayList<>())
+                        .add(libro);
+                return true;
+
+        }
 }
